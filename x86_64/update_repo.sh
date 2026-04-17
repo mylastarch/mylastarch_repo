e1e0cd04dd5913832a2e4e8cbf14aae8dfa921de
mylastarch <mylastarch@dccathome.com>
2026-04-16 23:35:45 -0400

    first commit

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm mylastarch_repo*
+
+echo "repo-add"
+repo-add -s -n -R mylastarch_repo.db.tar.gz *.pkg.tar.zst
+sleep 5
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
