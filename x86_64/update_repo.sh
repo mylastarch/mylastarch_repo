2c9587235deb3c11574eba0edb388bffd22f209c
mylastarch <mylastarch@dccathome.com>
2026-05-21 00:01:19 -0400

    rebuilt without sig

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -8,16 +8,16 @@ repo-add -n -R mylastarch_repo.db.tar.gz *.pkg.tar.zst
 sleep 1
 
 rm mylastarch_repo.db
-rm mylastarch_repo.db.sig
+#rm mylastarch_repo.db.sig
 
 rm mylastarch_repo.files
-rm mylastarch_repo.files.sig
+#rm mylastarch_repo.files.sig
 
 mv mylastarch_repo.db.tar.gz mylastarch_repo.db
-mv mylastarch_repo.db.tar.gz.sig mylastarch_repo.db.sig
+#mv mylastarch_repo.db.tar.gz.sig mylastarch_repo.db.sig
 
 mv mylastarch_repo.files.tar.gz mylastarch_repo.files
-mv mylastarch_repo.files.rar.gz.sig mylastarch_repo.files.sig
+#mv mylastarch_repo.files.rar.gz.sig mylastarch_repo.files.sig
 
 
 echo "####################################"
